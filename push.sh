7ee8384f8753e60fc726c7cb3ce268335a086675
Neokrat <neokrat@gmail.com>
2024-02-01 15:19:39 +0100

    2024-02-01-15-19-39

--- push.sh
+++ push.sh
@@ -6,6 +6,4 @@ commit_message=$(date +"%Y-%m-%d-%H-%M-%S")
 
 git add .
 git commit -m $commit_message
-#git push https://$GIT_USERNAME:$GIT_APP_PASSWORD@bitbucket.org/${service.git_repository}.git
 git push https://$GIT_USERNAME:$GIT_APP_PASSWORD@$GIT_REPOSITORY
-
