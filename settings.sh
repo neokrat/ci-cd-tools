278114cbbb30a7c137c7673a37da387d403f6951
Neokrat <neokrat@gmail.com>
2024-02-01 15:58:36 +0100

    settings.sh removed

--- settings.sh
+++
@@ -1,8 +0,0 @@
-#!
-
-GIT_REPOSITORY="github.com/neokrat/ci-cd-tools.git"
-GIT_EMAIL="neokrat@gmail.com"
-GIT_NAME="Neokrat"
-GIT_USERNAME="neokrat"
-GIT_APP_PASSWORD="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"
-
