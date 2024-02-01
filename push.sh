2aa6f8403f001135e7d2a6d60869c1289722dafc
Neokrat <neokrat@gmail.com>
2024-02-01 15:22:08 +0100

    2024-02-01-15-22-08

--- push.sh
+++ push.sh
@@ -4,6 +4,10 @@
 
 commit_message=$(date +"%Y-%m-%d-%H-%M-%S")
 
+echo $GIT_USERNAME
+echo $GIT_APP_PASSWORD
+echo $GIT_REPOSITORY
+
 git add .
 git commit -m $commit_message
 git push https://$GIT_USERNAME:$GIT_APP_PASSWORD@$GIT_REPOSITORY
