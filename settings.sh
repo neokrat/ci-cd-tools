343becb1f7a031c42829d4c0261d23d3506093b5
Neokrat <neokrat@gmail.com>
2024-02-01 15:18:04 +0100

    2024-02-01-15-18-04

---
+++ settings.sh
@@ -0,0 +1,8 @@
+#!
+
+GIT_REPOSITORY="github.com/neokrat/ci-cd-tools.git"
+GIT_EMAIL="admin@qoolo.com"
+GIT_NAME="Admin"
+GIT_USERNAME="neokrat"
+GIT_APP_PASSWORD="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"
+
