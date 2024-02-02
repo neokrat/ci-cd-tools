75f846f2486d1c3752ac3ecc86578c3091b92498
Neokrat <neokrat@gmail.com>
2024-02-02 12:41:31 +0100

    Changed add-testing-branch.sh.

--- add-testing-branch-00.sh
+++ add-testing-branch-00.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-branch_prefix="testing"
+branch_prefix=testing
 current_branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-
-# Check if we're already on a branch that starts with the branch_prefix
-if [[ "$current_branch" == "$branch_prefix"* ]]; then
-  echo "You're currently on a testing branch ($current_branch)."
-  echo "Please switch to the original feature branch to perform this operation."
-  exit 1
-fi
 
 if [ -n "$current_branch" ]; then
   if [ "$current_branch" == "master" ]; then
@@ -22,14 +15,15 @@ if [ -n "$current_branch" ]; then
     if git show-ref --verify --quiet "refs/heads/$new_branch"; then
       echo "The branch '$new_branch' already exists."
       git checkout "$new_branch"
-      echo "You've been switched to this branch: '$new_branch',"
+      echo "You've been switched to this branch '$branch_prefix/$current_branch',"
       echo "branched from the original feature branch '$current_branch'."
     else
       git checkout -b "$new_branch"
-      echo "A new branch for testing ('$new_branch') has been created and checked out,"
+      echo "A new '$new_branch' feature branch has been created."
+      echo "You've been switched to this branch '$branch_prefix/$current_branch',"
       echo "branched from the original feature branch '$current_branch'."
     fi
   fi
 else
-  echo "Not on any Git branch."
+  echo "Not on any Git branch"
 fi
